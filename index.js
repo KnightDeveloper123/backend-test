043f8e2c34401e4718d7103a4c525c8b12427600
KnightDeveloper123 <KnightDeveloper123>
2023-12-27 12:45:28 +0530

    New APIs added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const page  = require('./routes/page')
 const app = express()
 const port = 5941
 
-app.use('/', page)
+app.use('/page/', page)
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
 })
--- routes/page.js
+++ routes/page.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/test', (req, res)=>{
-    res.json({message:"Hello is message, and you have deployed APIs"})
+    res.json({message:"This is Test API"})
+})
+
+router.get('/hello', (req, res)=>{
+    res.json({message:"This is hello API"})
 })
 
 module.exports = router;
