f55975352d58340e56163003a44a0479e82b322b
KnightDeveloper123 <KnightDeveloper123>
2023-12-29 14:57:34 +0530

    fixed body issue

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const page  = require('./routes/page')
 const app = express()
 const port = 5941
 var cors = require('cors')
+const bodyParser = require('body-parser');
 
 app.use(cors())
+app.use(express.json())
 
 app.use('/page/', page)
 app.listen(port, () => {
