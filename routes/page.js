a7668649580055900a61133a0bc2eb3b8c1cff1a
KnightDeveloper123 <KnightDeveloper123>
2023-12-29 12:45:36 +0530

    Added web push backend api

--- routes/page.js
+++ routes/page.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const connection = require('../db')
+const webpush = require('web-push');
 
 router.get('/test', (req, res)=>{
     res.json({message:"This is Test API"})
@@ -15,6 +16,24 @@ router.get('/database',async (req, res)=>{
     const result = await connection.query('select * from test')
     const data = result.rows
     res.json({data})
+})
+
+const publicVapidKey = "BPHcUQEW_OLPLwyiDJuK0POa2hNYClIr7boZHmbOmMjDnrr_NgzmM0_DQJU622sPFTYertotkv1tFmk1lFe7iNc";
+
+const privateVapidKey = "vg-qy-6cyW96Hhxbnj1r97c6_6K6H194jAHYTkyCGyg";
+
+// Setup the public and private VAPID keys to web-push library.
+webpush.setVapidDetails("mailto: <thorragnorak241077@gmail.com>", publicVapidKey, privateVapidKey);
+
+// Create route for allow client to subscribe to push notification.
+router.post('/subscribe', (req, res) => {
+    const subscription = req.body;
+    res.status(201).json({});
+    const payload = JSON.stringify({ title: "Hello World", body: "This is your first push notification" });
+
+    webpush.sendNotification(subscription, payload).catch(console.log);
+
+    // res.send("hello");
 })
 
 module.exports = router;
