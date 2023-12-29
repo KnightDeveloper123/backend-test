77c7ca9e5c4369c29210f023bbd8f1edcfb0e0e9
KnightDeveloper123 <KnightDeveloper123>
2023-12-29 13:18:52 +0530

    cors policy added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const page  = require('./routes/page')
 const app = express()
 const port = 5941
+var cors = require('cors')
+
+app.use(cors())
 
 app.use('/page/', page)
 app.listen(port, () => {
