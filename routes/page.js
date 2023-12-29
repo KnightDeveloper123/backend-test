16b13fda92b0384475d1b78ae5ed39e16b1b163c
KnightDeveloper123 <KnightDeveloper123>
2023-12-29 12:50:32 +0530

    Bug fix

--- routes/page.js
+++ routes/page.js
@@ -31,7 +31,9 @@ router.post('/subscribe', (req, res) => {
     res.status(201).json({});
     const payload = JSON.stringify({ title: "Hello World", body: "This is your first push notification" });
 
-    webpush.sendNotification(subscription, payload).catch(console.log);
+    webpush.sendNotification(subscription, payload).catch(err=>{
+        res.json({err:"You have got an err"})
+    });
 
     // res.send("hello");
 })
